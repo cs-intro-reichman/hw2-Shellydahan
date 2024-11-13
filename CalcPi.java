5cb9c298984d68738ad0a7656b8fe4ca9601f09e
שלי דהן <slydhn@h-MacBook-Air-sl-sly.local>
2024-11-13 16:53:36 +0200

    calcPi1

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,19 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int terms = Integer.parseInt( args[0]);
+		double sum=0.0;
+		double times=3;
+		for(int i=1; i<terms; i++)
+		{
+		  if(i%2==0)
+		    sum= sum- (1/times);
+		  else
+		      sum=sum+(1/times);
+		  times=times+2;
+		}
+		sum= 1-sum;
+		System.out.println("pi according to Java: "+ Math.PI);
+		System.out.println("pi, approximated:     "+ sum*4);
 	}
 }
