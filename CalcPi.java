6e43a2ada5869c2e728ff092d5211c063dcd5f77
Shellydahan <shellydahan18@gmail.com>
2024-11-20 13:56:12 +0200

    final

--- CalcPi.java
+++ CalcPi.java
@@ -3,17 +3,16 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int terms = Integer.parseInt( args[0]);
-		double sum=0.0;
+		double sum=1.0;
 		double times=3.0;
 		for(int i=1; i<terms; i++)
 		{
-		  if(i%2==0)
-		    sum= sum-(1.0/times);
+		  if(i%2 == 0)
+		    sum= sum + (1.0/times);
 		  else
-		      sum=sum+(1.0/times);
+		      sum= sum - (1.0/times);
 		  times=times+2;
 		}
-		sum= 1-sum;
 		System.out.println("pi according to Java: "+ Math.PI);
 		System.out.println("pi, approximated:     "+ sum*4);
 	}
